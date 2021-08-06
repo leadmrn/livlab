35bffb193df2aad0871af23ec3d15f3ca8c68210
Léa De Amorin <leadeamorin.ps@gmail.com>
2021-08-06 15:45:20 +0200

    event ionInput

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,12 @@
         <ion-title>Moteur de recherche</ion-title>
       </ion-toolbar>
       <ion-toolbar>
-        <ion-searchbar placeholder="Rechercher un sport" showCancelButton="focus" animated></ion-searchbar>
+        <ion-searchbar placeholder="Rechercher un sport" showCancelButton="focus" @ionInput="ionInput($event.target.value)" animated></ion-searchbar>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <ion-list>
-        <ion-item v-for="sport of sports" v-bind:key="sport.id">{{ sport.name }}</ion-item>
+        <ion-item v-for="sport of sports" :key="sport.id">{{ sport.name }}</ion-item>
       </ion-list>
     </ion-content>
   </ion-page>
@@ -18,8 +18,19 @@
 
 <script>
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem } from '@ionic/vue';
+
 export default {
   name: 'Accueil',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
+    IonList,
+    IonItem
+  },
   data(){
     return{
       sports: [
@@ -34,18 +45,10 @@ export default {
       ]
     }
   },
-  handleChange(){
-    console.log('cc')
-  },
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    IonSearchbar,
-    IonList,
-    IonItem
+  methods : {
+    ionInput(value){
+      console.log(value);
+    }
   }
 };
 </script>
